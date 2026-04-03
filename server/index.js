fae411ad78e450b0b5ebb32d6b70a627e661dcce
Saarvik-got-it <saarvikxcode@gmail.com>
2026-04-03 11:14:47 +0530

    Reular Commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
-//URL Manipulation 
-
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const express = require('express');
 
-
-const myServer = http.createServer((req, res) => {          
-
-    if(req.url === '/favicon.ico') return res.end();
+//              <<<---HTTP Server--->>>
+function myHandler(req, res) {
+     if(req.url === '/favicon.ico') return res.end();
 
     const log = `${Date.now()} ${req.method} ${req.url} New Request Received \n`;
     const myUrl = url.parse(req.url, true)
@@ -27,11 +25,41 @@ const myServer = http.createServer((req, res) => {
         case '/search':
             const search = myUrl.query.search_query;
             res.end('Here are your results for ' + search);
+        case '/signup':
+            if(req.method === 'GET') res.end('This is the signup form');
+            else if(req.method === 'POST') {
+                //DB query
+                res.end('Login Successfull');
+            }
+            break;
         default:
             res.end('404 Not Found')
-    }
+    }   
     })
-});
+}
+
+// const myServer = http.createServer(myHandler);
+// myServer.listen(8000, () => console.log("Server Started ! "));
+//------------------------------------------------------------------------------------------------------------------------
 
 
-myServer.listen(8000, () => console.log("Server Started ! "));
+
+
+
+//              <<<---EXPRESS App--->>>
+
+//Creates an express app (similar to a handler function in HTTP server)
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("This is the home page");
+})
+
+app.get('/about', (req, res) => {
+    res.send(`I am ${req.query.name}`);
+})
+
+// const myServer = http.createServer(app);
+// myServer.listen(8000, () => console.log("Server Started ! "));
+
+app.listen(8000, () => console.log("Server Started !"));    
